575e3ae2e915be611ad8b5a0ca1bc7434976e6e2
Lalo <lalo.sa.94@gmail.com>
2014-07-29 16:27:58 -0500

    Cosas de ovi

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -90,7 +90,39 @@ public class PlayerScript : MonoBehaviour {
 			moveRight = Input.GetKey (KeyCode.RightArrow);
 
 			if (moveUp) {
-				if (colUp == null) {
+				if (colUp != null) {
+					if (colUp.CompareTag("Spider")) {
+						attacking = true;
+						time = timeMax;
+						anim.SetBool("AttackUp", true);
+						anim.SetBool("AttackDown", false);
+						anim.SetBool("AttackLeft", false);
+						anim.SetBool("AttackRight", false);
+						if(colUp.gameObject.GetComponent<SpiderScript>().getLife() > 1)
+							colUp.gameObject.GetComponent<SpiderScript>().reduceLife(1);
+						else {
+							Bounds spiderBounds = colUp.gameObject.GetComponent<SpriteRenderer>().bounds;
+							Destroy(colUp.gameObject);
+							AstarPath.active.UpdateGraphs(spiderBounds);
+						}
+					}
+					else if (colUp.CompareTag("Key")) {
+						KeyScript.dungeonKey = true;
+						Destroy (colUp.gameObject);
+					}
+					else if (colUp.CompareTag("Door") && KeyScript.dungeonKey) {
+						Application.LoadLevel(lvlChange);
+					}
+					else if (!colUp.CompareTag("Wall")) {
+						direction = 1;
+						time = timeMax;
+						anim.SetBool ("MoveUp", true);
+						anim.SetBool ("MoveDown", false);
+						anim.SetBool ("MoveLeft", false);
+						anim.SetBool ("MoveRight", false);
+					}
+				}
+				else {
 					direction = 1;
 					time = timeMax;
 					anim.SetBool ("MoveUp", true);
@@ -98,31 +130,41 @@ public class PlayerScript : MonoBehaviour {
 					anim.SetBool ("MoveLeft", false);
 					anim.SetBool ("MoveRight", false);
 				}
-				else if (colUp.CompareTag("Spider")) {
-					attacking = true;
-					time = timeMax;
-					anim.SetBool("AttackUp", true);
-					anim.SetBool("AttackDown", false);
-					anim.SetBool("AttackLeft", false);
-					anim.SetBool("AttackRight", false);
-					if(colUp.gameObject.GetComponent<SpiderScript>().getLife() > 1)
-						colUp.gameObject.GetComponent<SpiderScript>().reduceLife(1);
-					else {
-						Bounds spiderBounds = colUp.gameObject.GetComponent<SpriteRenderer>().bounds;
-						Destroy(colUp.gameObject);
-						AstarPath.active.UpdateGraphs(spiderBounds);
-					}
-				}
-				else if (colUp.CompareTag("Key")) {
-					KeyScript.dungeonKey = true;
-					Destroy (colUp.gameObject);
-				}
-				else if(colUp.CompareTag("Door") && KeyScript.dungeonKey)
-				{	Application.LoadLevel(lvlChange);}
-
 			}
 			else if (moveDown) {
-				if (colDown == null) {
+				if (colDown != null) {
+					if (colDown.CompareTag("Spider")) {
+						attacking = true;
+						time = timeMax;
+						anim.SetBool("AttackUp", false);
+						anim.SetBool("AttackDown", true);
+						anim.SetBool("AttackLeft", false);
+						anim.SetBool("AttackRight", false);
+						if(colDown.gameObject.GetComponent<SpiderScript>().getLife() > 1)
+							colDown.gameObject.GetComponent<SpiderScript>().reduceLife(1);
+						else {
+							Bounds spiderBounds = colDown.gameObject.GetComponent<SpriteRenderer>().bounds;
+							Destroy(colDown.gameObject);
+							AstarPath.active.UpdateGraphs(spiderBounds);
+						}
+					}
+					else if (colDown.CompareTag("Key")) {
+						KeyScript.dungeonKey = true;
+						Destroy (colDown.gameObject);
+					}
+					else if (colDown.CompareTag("Door") && KeyScript.dungeonKey) {
+						Application.LoadLevel(lvlChange);
+					}
+					else if (!colDown.CompareTag("Wall")) {
+						direction = 2;
+						time = timeMax;
+						anim.SetBool ("MoveUp", false);
+						anim.SetBool ("MoveDown", true);
+						anim.SetBool ("MoveLeft", false);
+						anim.SetBool ("MoveRight", false);
+					}
+				}
+				else {
 					direction = 2;
 					time = timeMax;
 					anim.SetBool ("MoveUp", false);
@@ -130,31 +172,41 @@ public class PlayerScript : MonoBehaviour {
 					anim.SetBool ("MoveLeft", false);
 					anim.SetBool ("MoveRight", false);
 				}
-				else if (colDown.CompareTag("Spider")) {
-					attacking = true;
-					time = timeMax;
-					anim.SetBool("AttackUp", false);
-					anim.SetBool("AttackDown", true);
-					anim.SetBool("AttackLeft", false);
-					anim.SetBool("AttackRight", false);
-					if(colDown.gameObject.GetComponent<SpiderScript>().getLife() > 1)
-						colDown.gameObject.GetComponent<SpiderScript>().reduceLife(1);
-					else {
-						Bounds spiderBounds = colDown.gameObject.GetComponent<SpriteRenderer>().bounds;
-						Destroy(colDown.gameObject);
-						AstarPath.active.UpdateGraphs(spiderBounds);
-					}
-				}
-				else if (colDown.CompareTag("Key")) {
-					KeyScript.dungeonKey = true;
-					Destroy (colDown.gameObject);
-				}
-				else if(colDown.CompareTag("Door") && KeyScript.dungeonKey)
-				{	Application.LoadLevel(lvlChange);}
-
 			}
 			else if (moveLeft) {
-				if (colLeft == null) {
+				if (colLeft != null) {
+					if (colLeft.CompareTag("Spider")) {
+						attacking = true;
+						time = timeMax;
+						anim.SetBool("AttackUp", false);
+						anim.SetBool("AttackDown", false);
+						anim.SetBool("AttackLeft", true);
+						anim.SetBool("AttackRight", false);
+						if(colLeft.gameObject.GetComponent<SpiderScript>().getLife() > 1)
+							colLeft.gameObject.GetComponent<SpiderScript>().reduceLife(1);
+						else {
+							Bounds spiderBounds = colLeft.gameObject.GetComponent<SpriteRenderer>().bounds;
+							Destroy(colLeft.gameObject);
+							AstarPath.active.UpdateGraphs(spiderBounds);
+						}
+					}
+					else if (colLeft.CompareTag("Key")) {
+						KeyScript.dungeonKey = true;
+						Destroy (colLeft.gameObject);
+					}
+					else if (colLeft.CompareTag("Door") && KeyScript.dungeonKey) {
+						Application.LoadLevel(lvlChange);
+					}
+					else if (!colLeft.CompareTag("Wall")) {
+						direction = 3;
+						time = timeMax;
+						anim.SetBool ("MoveUp", false);
+						anim.SetBool ("MoveDown", false);
+						anim.SetBool ("MoveLeft", true);
+						anim.SetBool ("MoveRight", false);
+					}
+				}
+				else {
 					direction = 3;
 					time = timeMax;
 					anim.SetBool ("MoveUp", false);
@@ -162,30 +214,41 @@ public class PlayerScript : MonoBehaviour {
 					anim.SetBool ("MoveLeft", true);
 					anim.SetBool ("MoveRight", false);
 				}
-				else if (colLeft.CompareTag("Spider")) {
-					attacking = true;
-					time = timeMax;
-					anim.SetBool("AttackUp", false);
-					anim.SetBool("AttackDown", false);
-					anim.SetBool("AttackLeft", true);
-					anim.SetBool("AttackRight", false);
-					if(colLeft.gameObject.GetComponent<SpiderScript>().getLife() > 1)
-						colLeft.gameObject.GetComponent<SpiderScript>().reduceLife(1);
-					else {
-						Bounds spiderBounds = colLeft.gameObject.GetComponent<SpriteRenderer>().bounds;
-						Destroy(colLeft.gameObject);
-						AstarPath.active.UpdateGraphs(spiderBounds);
-					}
-				}
-				else if (colLeft.CompareTag("Key")) {
-					KeyScript.dungeonKey = true;
-					Destroy (colLeft.gameObject);
-				}
-				else if(colLeft.CompareTag("Door") && KeyScript.dungeonKey)
-				{	Application.LoadLevel(lvlChange);}
 			}
 			else if (moveRight) {
-				if (colRight == null) {
+				if (colRight != null) {
+					if (colRight.CompareTag("Spider")) {
+						attacking = true;
+						time = timeMax;
+						anim.SetBool("AttackUp", false);
+						anim.SetBool("AttackDown", false);
+						anim.SetBool("AttackLeft", false);
+						anim.SetBool("AttackRight", true);
+						if(colRight.gameObject.GetComponent<SpiderScript>().getLife() > 1 )
+							colRight.gameObject.GetComponent<SpiderScript>().reduceLife(1);
+						else {
+							Bounds spiderBounds = colRight.gameObject.GetComponent<SpriteRenderer>().bounds;
+							Destroy(colRight.gameObject);
+							AstarPath.active.UpdateGraphs(spiderBounds);; 
+						}
+					}
+					else if (colRight.CompareTag("Key")) {
+						KeyScript.dungeonKey = true;
+						Destroy (colRight.gameObject);
+					}
+					else if (colRight.CompareTag("Door") && KeyScript.dungeonKey) {
+						Application.LoadLevel(lvlChange);
+					}
+					else if (!colRight.CompareTag("Wall")) {
+						direction = 4;
+						time = timeMax;
+						anim.SetBool ("MoveUp", false);
+						anim.SetBool ("MoveDown", false);
+						anim.SetBool ("MoveLeft", false);
+						anim.SetBool ("MoveRight", true);
+					}
+				}
+				else {
 					direction = 4;
 					time = timeMax;
 					anim.SetBool ("MoveUp", false);
@@ -193,27 +256,6 @@ public class PlayerScript : MonoBehaviour {
 					anim.SetBool ("MoveLeft", false);
 					anim.SetBool ("MoveRight", true);
 				}
-				else if (colRight.CompareTag("Spider")) {
-					attacking = true;
-					time = timeMax;
-					anim.SetBool("AttackUp", false);
-					anim.SetBool("AttackDown", false);
-					anim.SetBool("AttackLeft", false);
-					anim.SetBool("AttackRight", true);
-					if(colRight.gameObject.GetComponent<SpiderScript>().getLife() > 1 )
-						colRight.gameObject.GetComponent<SpiderScript>().reduceLife(1);
-					else {
-						Bounds spiderBounds = colRight.gameObject.GetComponent<SpriteRenderer>().bounds;
-						Destroy(colRight.gameObject);
-						AstarPath.active.UpdateGraphs(spiderBounds);; 
-					}
-				}
-				else if (colRight.CompareTag("Key")) {
-					KeyScript.dungeonKey = true;
-					Destroy (colRight.gameObject);
-				}
-				else if(colRight.CompareTag("Door") && KeyScript.dungeonKey)
-				{	Application.LoadLevel(lvlChange);}
 			}
 		} else if (turn) {
 			switch (direction) {
